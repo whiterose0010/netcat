5f8646a0e8d1950f520fca2277da6690b14c0f2f
verm333r <botnet.inbox@gmail.com>
2024-09-21 00:09:52 +0800

    menyempurnakan file

--- netcat.py
+++ netcat.py
@@ -1,120 +1,73 @@
-import argparse
 import socket
-import shlex
-import subprocess
-import sys
-import textwrap
 import threading
+import sys
 
-def execute(cmd):
-    cmd = cmd.strip()
-    if not cmd:
-        return 
-    output = subprocess.check_output(shlex.split(cmd), stderr = subprocess.STDOUT)
-    return output.decode()
-
-class NetCat:
-    def __init__(self, args, buffer=None):
-        self.args = args
-        self.buffer = buffer
-        
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    def run(self):
-        if self.args.listen:
-            self.listen()
-        else:
-            self.send()
-
-    def send(self):
-        self.socket.connect((self.args.target, self.args.port))
-        if self.buffer:
-            self.socket.send(self.buffer)
-
-        try:
-            while True:
-                recv_len = 1
-                response = ''
-                while recv_len:
-                    data = self.socket.recv(4096)
-                    recv_len = len(data)
-                    response += data.decode()
-                    if recv_len < 4096 :
-                        break
-                    if response:
-                        print(response)
-                        buffer = input('> ')
-                        buffer += '\n'
-                        self.socket.send(buffer.encode())
-        except KeyboardInterrupt:
-            print('User terminated.')
-            self.socket.close()
-            sys.exit()
-
-    def listen(self):
-        self.socket.bind((self.args.target, self.args.port))
-        self.socket.listen(5)
+def handle_client(client_socket):
+    with client_socket:
         while True:
-            client_socket, _ = self.socket.accept()
-            client_thread = threading.Thread(
-                target=self.handle, args=(client_socket,)
-            )
-            client_thread.start()
-
-    def handle(self, client_socket):
-        if self.args.execute:
-            output = execute(self.args.execute)
-            client_socket.send(output.encode())
-        elif self.args.upload:
-            file_buffer = b''
-            while True:
-                data = client_socket.recv(4096)
-                if data :
-                    file_buffer +=data
-                else:
+            try :
+                request = client_socket.recv(4096)
+                if not request:
+                    print("connection closed by client.")
                     break
-                with open(self.args.upload, 'wb') as f:
-                    f.write(file_buffer)
-                message = f'Saved file {self.args.upload}'
-                client_socket.send(message.encode())
-        elif self.args.command:
-            cmd_buffer = b''
-            while True:
-                try:
-                    client_socket.send(b'BHP: #> ')
-                    while '\n' not in cmd_buffer.decode():
-                        cmd_buffer += client_socket.recv(64)
-                        response = execute(cmd_buffer.decode())
-                    if response:
-                        client_socket.send(response.encode())
-                        cmd_buffer = b''
-                except Exception as e:
-                    print(f'server killed {e}')
-                    self.socket.close()
-                    sys.exit()
 
-if __name__=='__main__':
-    parser = argparse.ArgumentParser(
-        description='BHP Net Tool',
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog=textwrap.dedent('''Example:
-                               netcat.py -t 192.168.1.108 -p 5555 -l -c  # example command shell
-                               netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
-                               netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat/etc/passwd\" # execute command
-                               echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
-                               netcat.py -t 192.168.1.108 -p 5555 # connect to server'''))
-    parser.add_argument('-c', '--command', action='store_true', help='command shell')
-    parser.add_argument('-e', '--execute', help='execute specified command')
-    parser.add_argument('-l', '--listen', action='store_true', help='listen')
-    parser.add_argument('-p', '--port', type=int, default = 5555, help='specified port')
-    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
-    parser.add_argument('-u', '--upload', help='upload file')
-    args= parser.parse_args()
-    if args.listen:
-        buffer = ''
-    else :
-        buffer = sys.stdin.read()
+                print(f"Received : {request.decode()}")
+                response = "Echo : " + request.decode()
+                client_socket.send(response.encode())
+            except Exception as e:
+                print(f"Error : {e}")
+                break
 
-    nc = NetCat(args, buffer.encode())
-    nc.run()
+def server(host, port):
+    server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+    print(f"Listening on {host}:{port}")
+
+    while True:
+        try:
+            client_socket, addr = server_socket.accept()
+            print(f"Accepted connection from {addr}")
+            client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+            client_handler.start()
+        except Exception as e:
+            print(f"Error accepting connection : {e}")
+
+def client(target_host, target_port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client_socket.connect((target_host,target_port))
+    except Exception as e:
+        print(f"Error connecting to server : {e}")
+        return
+
+    try:
+        while True:
+            message = input("Send (type 'exit' to quit) : ")
+            if message.lower() == 'exit':
+                print("Closing connection.")
+                break
+            client_socket.send(message.encode())
+            response = client_socket.recv(4096)
+            print(f"Received: {response.decode()}")
+    except KeyboardInterrupt:
+        print("\nConnection closed by user")
+    except Exception as e:
+        print(f"Error : {e}")
+    finally:
+        client_socket.close()
+        print("Client socket closed.")
+
+if __name__ == "__main__":
+    mode = input("Run as server (s) or client (c)?")
+
+    if mode.lower() == 's':
+        host = input("Enter host to listen on : ")
+        port = int(input("Enter port to listen on : "))
+        server(host,port)
+    elif mode.lower() == 'c':
+        target_host = input("Enter target host : ")
+        target_port = int(input("Enter target port : "))
+        client(target_host,target_port)
+    else:
+        print("Invalid option. Please choose 's' or 'c'.")
